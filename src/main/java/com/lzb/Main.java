e5b0b35a24bfcea25198dfc780e0df08930af43d
chenpi <chenpi@sinoxk.com>
2021-03-29 11:22:57 +0800

    新增主方法

---
+++ src/main/java/com/lzb/Main.java
@@ -0,0 +1,13 @@
+package com.lzb;
+/**
+ * 主方法<br/>
+ * Created on : 2021-03-29 11:19
+ * @author chenpi 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+
+}
