8fef94db5f43ca5bd07d8417cd12d5d94b2b7e73
chenpi <chenpi@sinoxk.com>
2021-05-17 11:44:58 +0800

    线程中断测试

---
+++ src/main/java/com/lzb/concurrent/thread_interrupt/Test.java
@@ -0,0 +1,43 @@
+package com.lzb.concurrent.thread_interrupt;
+/**
+ * sleep 被中断，抛异常，会自动重置中断标志位，isInterrupted 是 false
+ * 如果要中断线程，可以调用：Thread.currentThread().interrupt()；
+ * 如果要返回当前中断标志位，并复位：Thread.interrupted()
+ *
+ * Created on : 2021-05-17 10:49
+ * @author chenpi 
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("线程被中断了，退出");
+                    break;
+                }
+                System.out.println("1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    try {
+                        Thread.sleep(100000);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    e.printStackTrace();
+                    System.out.println("sleep 被中断");
+                    //Thread.currentThread().interrupt();
+                }
+            }
+        });
+        t.start();
+        try {
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //t.interrupt();
+    }
+
+}
