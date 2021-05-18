b5f23f1b918b27f35fe106c09878f592f86cf4ff
chenpi <chenpi@sinoxk.com>
2021-05-18 14:49:48 +0800

    增加测试类

---
+++ src/test/java/com/lzb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzb;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/lzb/manager/OrderDetailQueueManagerTest.java
@@ -0,0 +1,81 @@
+package com.lzb.manager;
+
+import com.lzb.ApplicationTests;
+import com.lzb.queue.OrderDetailQueueManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * 队列管理器测试<br/>
+ * Created on : 2021-05-18 10:55
+ * @author chenpi 
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationTests.class)
+public class OrderDetailQueueManagerTest {
+
+    @Resource
+    private OrderDetailQueueManager orderDetailQueueManager;
+
+    @Test
+    public void test() throws Exception {
+        int threadSize = 5;
+
+        //每个线程生成1-max个数，随机放到redis队列，所有线程执行完，redis队列只有max个元素
+        int max = 1000;
+        CountDownLatch readCdl = new CountDownLatch(threadSize);
+        for (int i = 0; i < threadSize; i++) {
+            Thread push = new Thread(() -> {
+                try {
+                    List<Integer> numList = new ArrayList<>(max);
+                    for (int num = 1; num <= max; num++) {
+                        numList.add(num);
+                    }
+                    //乱序，增加冲突
+                    Collections.shuffle(numList);
+                    numList.forEach(num -> {
+                        orderDetailQueueManager.push(Objects.toString(num), Objects.toString(num));
+                    });
+                } finally {
+                    readCdl.countDown();
+                }
+            });
+            push.start();
+        }
+        readCdl.await();
+        System.out.println("push完毕，队列个数:" + orderDetailQueueManager.size());
+
+        LongAdder sum = new LongAdder();
+        for (int i = 0; i < threadSize; i++) {
+            Thread pop = new Thread(() -> {
+                while (true) {
+                    orderDetailQueueManager.blockingPop().map(Long::valueOf).ifPresent(sum::add);
+                }
+            });
+            pop.start();
+        }
+
+        //每隔一秒钟打印sum
+        Thread printSum = new Thread(() -> {
+           while (true) {
+               try {
+                   Thread.sleep(1000);
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+               System.out.println(sum.longValue());
+           }
+        });
+        printSum.start();
+        printSum.join();
+
+    }
+
+}
