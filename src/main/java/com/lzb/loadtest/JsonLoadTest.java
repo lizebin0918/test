424520c0e45dcf7fbce4cc8d9711b4c52af8dad7
lizebin <lizebin0918@gmail.com>
2021-05-20 21:51:52 +0800

    取值方法修改

--- src/main/java/com/lzb/loadtest/JsonLoadTest.java
+++ src/main/java/com/lzb/loadtest/JsonLoadTest.java
@@ -1,6 +1,8 @@
 package com.lzb.loadtest;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
@@ -24,7 +26,7 @@ public class JsonLoadTest {
     public static void main(String[] args) throws InterruptedException {
         final String jsonString = "{\"buyDate\":\"1617552000\",\"payMoney\":\"0\",\"storeNo\":\"81104\",\"orderNo\":\"100432293097765048\",\"name\":\"片仔癀\",\"count\":\"1.0\",\"drugId\":\"107231\",\"costPrice\":\"476.00\",\"originMoney\":\"590.00\",\"retailPrice\":\"590.00\",\"costAmount\":\"476.00\",\"grossProfitAmount\":\"97.30\"}";
 
-        int threadSize = 500;
+        int threadSize = 1000;
         CountDownLatch latch = new CountDownLatch(threadSize);
         for (int i=0; i<threadSize; i++) {
             Thread thread = new Thread(() -> {
@@ -42,8 +44,16 @@ public class JsonLoadTest {
 
                 //fastjson
                 {
-                    JSON.parseObject(jsonString, OrderDetailDTO.class, global);
+                    /*System.out.println(JSON.parseObject(jsonString, OrderDetailDTO.class,
+                                                        Feature.DisableCircularReferenceDetect,
+                                                        Feature.DisableFieldSmartMatch,
+                                                        Feature.DisableSpecialKeyDetect).getBuyDate());*/
                     //System.out.println(threadName + fastjsonString);
+                    System.out.println(JSONObject.parseObject(jsonString,
+                                           Feature.DisableCircularReferenceDetect,
+                                           Feature.IgnoreNotMatch,
+                                           Feature.DisableFieldSmartMatch,
+                                           Feature.DisableSpecialKeyDetect).getString("buyDate"));
                 }
 
                 //gson
@@ -51,8 +61,17 @@ public class JsonLoadTest {
                     //gson.fromJson(jsonString, OrderDetailDTO.class);
                     //String gsonString = OrderDetailDTO.fromJson(jsonString).toJson();
                     //System.out.println(gsonString);
+
+                    //OrderDetailDTO.fromJson(jsonString);
                 }
 
+                //gson parser
+                {
+                    /*JsonObject o = JsonParser.parseString(jsonString).getAsJsonObject();
+                    if (o.has("buyDate")) {
+                        System.out.println(o.get("buyDate").toString());
+                    }*/
+                }
 
                 long end = System.currentTimeMillis();
                 System.out.println(threadName + "----->耗时(毫秒):" + (end - start));
