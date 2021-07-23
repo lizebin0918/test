206984080fceee6eb007796e744ae3f1dba50ff6
chenpi <chenpi@sinoxk.com>
2021-07-23 09:30:14 +0800

    测试jdk9新特性

---
+++ src/main/java/com/lzb/jdk14/Test.java
@@ -0,0 +1,46 @@
+package com.lzb.jdk14;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 新特性<br/>
+ * Created on : 2021-07-22 16:15
+ *
+ * @author chenpi
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        var i = 1;
+        System.out.println(i);
+
+        //list--------------------->
+        var intList1 = List.of(i, 2, 3);
+        var intList2 = new ArrayList<String>();
+        intList2.add("1");
+        intList2.add("2");
+        intList2.add("3");
+        System.out.println(intList1);
+        String[] array = intList2.toArray(String[]::new);
+        System.out.println(intList2);
+        System.out.println(Arrays.toString(array));
+        //list<---------------------
+
+        //Predicate
+        System.out.println(intList1.stream().filter(Predicate.not(item -> item == 2)).collect(Collectors.toList()));
+        System.out.println(intList1.stream().filter(Predicate.not(Objects::isNull)).collect(Collectors.toList()));
+
+        //String
+        System.out.println(intList2.stream().filter(Predicate.not(String::isBlank)).collect(Collectors.toList()));
+
+        //Stream
+        Stream.of("a", "b", "c", "", "e", "f").takeWhile(s->!s.isEmpty())
+            .forEach(System.out::print);
+        Stream.of("a","b","c","","e","f").dropWhile(s-> !s.isEmpty())
+            .forEach(System.out::print);
+    }
+
+}
