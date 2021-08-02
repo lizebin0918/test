f1d11a5108477379551fd5b08df19a37215ea301
lizebin <lizebin0918@gmail.com>
2021-08-02 21:44:22 +0800

    goto 测试

---
+++ src/main/java/com/lzb/goto_test/TestGoto.java
@@ -0,0 +1,36 @@
+package com.lzb.goto_test;
+
+/**
+ * break out;跳出out对应层的逻辑，继续执行后续逻辑<br/>
+ * Created on : 2021-08-02 21:34
+ *
+ * @author lizebin
+ */
+public class TestGoto {
+
+    public static void main(String[] args) {
+        //多层循环，跳到指定的那一层循环
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            out:
+            for (; ; ) {
+                for (; ; ) {
+                    System.out.println(1);
+                    break out;
+                }
+            }
+        }*/
+
+        //if-else
+        o:
+        while (true) {
+            System.out.println("1");
+            if (args.length == 0) {
+                break o;
+            }
+            System.out.println("2");
+        }
+        System.out.println("3");
+    }
+
+}
