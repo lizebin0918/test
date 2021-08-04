37ef5ffc38ade53c9086d5a7f552f47e1a4e86fe
lizebin <lizebin0918@gmail.com>
2021-08-04 08:28:50 +0800

    feature:二进制测试

--- src/main/java/com/lzb/jdk/TestBinary.java
+++ src/main/java/com/lzb/jdk/TestBinary.java
@@ -8,6 +8,9 @@ package com.lzb.jdk;
  */
 public class TestBinary {
 
+    private static final int COUNT_BITS = Integer.SIZE - 3;
+    private static final int COUNT_MASK = (1 << COUNT_BITS) - 1;
+
     public static void main(String[] args) {
         // 输出的是补码：11111111111111111111111111111111
         System.out.println(Integer.toBinaryString(-1));
@@ -19,6 +22,10 @@ public class TestBinary {
         System.out.println(Integer.toBinaryString(i));
         System.out.println(Integer.toBinaryString(15));
         System.out.println(15 & i);
+
+        System.out.println(Integer.toBinaryString(COUNT_MASK));
+        //正数的补码等于它自己，原码：01111111 11111111 11111111 11111111(31个1)
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
     }
 
 }
