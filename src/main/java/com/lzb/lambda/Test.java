958cfcdda4161ff4cc95b1787fcb2aef6cb966d1
zebin <bin.ze@shopcider.com>
2021-08-04 09:53:34 +0800

    add:split坑

---
+++ src/main/java/com/lzb/lambda/Test.java
@@ -0,0 +1,36 @@
+package com.lzb.lambda;
+
+/**
+ * <br/>
+ * Created on : 2021-07-30 16:28
+ *
+ * @author lizebin
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        var user = new User(666);
+
+        Object o = (Runnable)user::getId;
+        Runnable o1 = () -> {
+            System.out.println("runnable");
+        };
+
+        System.out.println(o);
+        System.out.println(o1);
+        System.out.println(o.getClass());
+        System.out.println(o1.getClass());
+
+        Thread.onSpinWait();
+
+    }
+    private static class User {
+        int id;
+        public User(int id) {
+            this.id = id;
+        }
+        public void getId() {
+            System.out.println(id);
+        }
+    }
+}
