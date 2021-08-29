097f67ce93443361bcb6907c2939a8286ed80888
lizebin <lizebin0918@gmail.com>
2021-08-29 12:06:25 +0800

    test

--- src/main/java/com/lzb/jdk/TestThreadLocal.java
+++ src/main/java/com/lzb/jdk/TestThreadLocal.java
@@ -7,7 +7,16 @@ import java.util.concurrent.Executors;
 
 /**
  * -Xmx10M
- * 线程销毁，ThreadLocal是否会内存溢出？<br/>
+ * 线程销毁，ThreadLocal是否会内存泄露？究竟是哪个对象泄露？？？<br/>
+ * 一、采用线程池会有可能会导致内存泄露，这里有几个点需要注意：
+ *   如果直接线程消亡，ThreadLocalMap会自动被回收，只讨论线程池的情况
+ *   我们接收的HTTP请求，用的是Tomcat的线程池
+ *   我们自定义的线程池
+ * 二、内存泄露？遗漏的是啥？
+ *   漏的是TL被回收了，但是Entry的value没有被回收（因为线程还活着），就算同一个线程对同一个TL做多次set()方法，旧的value会被覆盖，旧的value会被回收掉
+ *   如果是强引用，因为线程池长时间存在，Thread->ThreadLocalMap->Entry->Entry.key->ThreadLocal，这样会导致Entry无法回收，value就会被遗忘在内存中。
+ *   如果是弱引用，TL被回收了，只有弱引用，每次GC会把key的引用断掉，ThreadLocalMap在get/set的时候会把对应的Entry清空，这样也释放了value
+ * 三、什么情况下，TL会被回收？？？？这个点有点迷.....情况估计是有的，但是我们没遇到而已
  * Created on : 2021-08-13 13:49
  *
  * @author lizebin
@@ -36,23 +45,22 @@ public class TestThreadLocal {
             }
         }*/
 
-        /*{
-            ExecutorService threadPool = Executors.newCachedThreadPool();
+        {
+            ExecutorService threadPool = Executors.newSingleThreadExecutor();
 
-            for (int i = 0; i < times; i++) {
+            for (int i = 0; i < 100; i++) {
                 threadPool.execute(() -> {
-                    LOCAL.set(new byte[1024 * 1024]);
+                    LOCAL.set(new byte[2 * 1024 * 1024]);
                     // 没有remove会导致内存溢出
-                    //LOCAL.remove();
+                    // LOCAL.remove();
                 });
-                gc();
+                // gc();
             }
-
             Thread.sleep(1000);
             threadPool.shutdown();
-        }*/
+        }
 
-        InheritableThreadLocal<String> stringInheritableThreadLocal = new InheritableThreadLocal<>();
+        /*InheritableThreadLocal<String> stringInheritableThreadLocal = new InheritableThreadLocal<>();
         stringInheritableThreadLocal.set("InheritableThreadLocal string");
         for (int i = 0; i < 10; i++) {
             new Thread(() -> {
@@ -62,7 +70,7 @@ public class TestThreadLocal {
         }
 
         Thread.sleep(5000);
-        System.out.println("done");
+        System.out.println("done");*/
 
     }
 
