cef59648e964cca52df62749fd83421aa4fca5dc
lizebin <lizebin0918@gmail.com>
2023-04-15 15:14:51 +0800

    test:测试例子

--- src/test/java/ApprovalTest.java
+++ src/test/java/ApprovalTest.java
@@ -1,7 +1,12 @@
 import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
+import java.util.Map;
 
 import com.alibaba.fastjson.JSON;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.approvaltests.ApprovalUtilities;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
@@ -21,16 +26,53 @@ public class ApprovalTest {
 	}
 
 	@Test
+	void should_test_map() {
+		Map<String, String> map = Map.of("a", "a", "b", "b");
+		Approvals.verify(map);
+	}
+
+	/**
+	 * 直接toString()
+	 */
+	@Test
+	void should_test_person() {
+		Person p = new Person();
+		p.setAge(10);
+		p.setName("naem");
+		Approvals.verify(p);
+	}
+
+	@Test
 	void test_json() {
 		String[] names = {"Llewellyn", "James", "Dan", "Jason", "Katrina"};
 		Approvals.verify(JSON.toJSONString(names));
 	}
 
 	@Test
-	void test_give_coupon() {
+	void should_test_sysout_inline_method() {
 		ByteArrayOutputStream output = new ApprovalUtilities().writeSystemOutToStringBuffer();
 
+		PersonService personService = new PersonService();
+		personService.test(new Person(1, "name"));
+
 		Approvals.verify(SystemOutUtils.convert(output));
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	static class Person {
+		private int age;
+		private String name;
+	}
+
+	@Slf4j
+	static class PersonService {
+
+		public void test(Person person) {
+			System.out.println("this is a person age : " + person.getAge());
+		}
+
 	}
 
 }
