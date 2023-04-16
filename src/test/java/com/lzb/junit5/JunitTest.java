387c3d97f64b8ff9d4d3c8def5f65b33d88a882f
lizebin <lizebin0918@gmail.com>
2023-04-16 10:15:32 +0800

    test:例子

--- src/test/java/com/lzb/junit5/JunitTest.java
+++ src/test/java/com/lzb/junit5/JunitTest.java
@@ -1,7 +1,13 @@
 package com.lzb.junit5;
 
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.util.Strings;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 // 每个实例都会初始化
 // @TestInstance(TestInstance.Lifecycle.PER_METHOD)
-class MathUtilTest {
+class JunitTest {
 
     private int result = 5;
 
@@ -34,5 +40,25 @@ class MathUtilTest {
         result = result / 5;
         System.out.println("test_Devide(5,5) => " + result);
         assertEquals(1, result);
+    }
+
+    /**
+     * 动态参数测试
+     * @param input
+     * @param expected
+     */
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input, boolean expected) {
+        assertEquals(expected, Strings.isBlank(input));
+    }
+
+    private static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+                Arguments.of(null, true),
+                Arguments.of("", true),
+                Arguments.of("  ", true),
+                Arguments.of("not blank", false)
+        );
     }
 }
