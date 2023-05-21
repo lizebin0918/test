ba734ae4710edf95bc4e099fd5466e033c5f7b4e
lizebin <lizebin0918@gmail.com>
2023-05-21 10:47:50 +0800

    feat:添加注释

--- src/main/java/com/lzb/ddd/repository/BaseRepository.java
+++ src/main/java/com/lzb/ddd/repository/BaseRepository.java
@@ -1,11 +1,26 @@
 package com.lzb.ddd.repository;
 
+import javax.annotation.Resource;
+
+import lombok.Setter;
+
 /**
  * <br/>
  * Created on : 2023-05-21 09:53
  * @author lizebin
  */
+@Setter
 public abstract class BaseRepository<R> implements AddRepository<R> {
+
+    // 受保护变量才能设置进去，不能是private
+    /*@Resource
+    protected TransactionHelper transactionHelper;
+
+    @Resource
+    protected DomainEventSender domainEventSender;
+
+    @Resource
+    protected OperationLogSupport operationLogSupport;*/
 
     protected abstract Runnable doAdd(R r);
 
