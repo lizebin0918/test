1cc84878f0e2eba2fc87fc4f17aaf2712b3788b1
lizebin <lizebin0918@gmail.com>
2023-10-07 16:45:31 +0800

    feat:重试任务

--- src/main/java/com/lzb/concurrent/threadpool/TestScheduledThreadPoolExecutor.java
+++ src/main/java/com/lzb/concurrent/threadpool/TestScheduledThreadPoolExecutor.java
@@ -17,15 +17,17 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestScheduledThreadPoolExecutor {
 
+    static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(16);
+
     public static void main(String[] args) throws InterruptedException {
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(16);
         System.out.println(Thread.currentThread().getName() + "current time:" + LocalDateTime.now());
         // 延迟5秒后执行
         scheduledExecutorService.schedule(() -> System.out.println("delay execute :" + LocalDateTime.now()), 5, TimeUnit.SECONDS);
 
         // 按照时间点执行
         scheduledExecutorService.scheduleAtFixedRate(() -> {
-            System.out.println(Thread.currentThread().getName() + " execute at fixed rate : " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+            System.out.println(Thread.currentThread().getName() + " execute at fixed rate : " + LocalDateTime.now()
+                    .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
@@ -35,7 +37,8 @@ public class TestScheduledThreadPoolExecutor {
 
         // 按照任务执行 + 固定时间执行
         scheduledExecutorService.scheduleWithFixedDelay(() -> {
-            System.out.println(Thread.currentThread().getName() + " execute with fixed delay : " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+            System.out.println(Thread.currentThread().getName() + " execute with fixed delay : " + LocalDateTime.now()
+                    .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
@@ -43,11 +46,21 @@ public class TestScheduledThreadPoolExecutor {
             }
         }, 0, 1, TimeUnit.SECONDS);
 
+        execute(1, 3, 10);
+
         Thread.sleep(10000);
         System.out.println(Thread.currentThread().getName() + "done");
 
         scheduledExecutorService.shutdown();
 
+    }
+
+    private static void execute(int... seconds) {
+        for (int second : seconds) {
+            scheduledExecutorService.schedule(() -> {
+                System.out.println("execute " + second + " seconds later");
+            }, second, TimeUnit.SECONDS);
+        }
     }
 
 }
