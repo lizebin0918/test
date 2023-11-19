9f2ca10f5d2c97533bddf2ff45fc494c7eb89354
lizebin <lizebin0918@gmail.com>
2023-11-19 23:50:19 +0800

    doc:注释

--- src/main/java/com/lzb/vavr/TryTest.java
+++ src/main/java/com/lzb/vavr/TryTest.java
@@ -1,9 +1,15 @@
 package com.lzb.vavr;
 
 import io.vavr.control.Try;
+import org.checkerframework.checker.units.qual.A;
+
+import static com.google.common.base.Predicates.instanceOf;
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.Match;
 
 /**
- * <br/>
+ * Try 是一个一元容器类型，它表示可能导致异常或返回成功计算值的计算。它与 Either 类似，但语义上不同。 Try 的实例是 Success 或 Failure 的实例。<br/>
  * Created on : 2023-01-13 19:44
  * @author lizebin
  */
@@ -26,6 +32,19 @@ public class TryTest {
                 .andThen(r -> System.out.println("and then 2 = " + r))
                 .get();
         System.out.println(result);
+
+        // 还能这样表达
+        /*A result = Try.of(this::bunchOfWork)
+                .recover(x -> Match(x).of(
+                        Case($(instanceOf(Exception_1.class)), t -> somethingWithException(t)),
+                        Case($(instanceOf(Exception_2.class)), t -> somethingWithException(t)),
+                        Case($(instanceOf(Exception_n.class)), t -> somethingWithException(t))
+                ))
+                .getOrElse(other);*/
+
+    }
+
+    private static void bunchOfWork() {
 
     }
 
