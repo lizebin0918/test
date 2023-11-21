448999c456a6e4ebc480ca8a810969db9dbea62e
lizebin <lizebin0918@gmail.com>
2023-11-21 08:33:30 +0800

    feat:模式匹配

--- src/main/java/com/lzb/vavr/MatcherTest.java
+++ src/main/java/com/lzb/vavr/MatcherTest.java
@@ -1,12 +1,15 @@
 package com.lzb.vavr;
 
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 import io.vavr.control.Option;
 
 import static io.vavr.API.$;
 import static io.vavr.API.Case;
 import static io.vavr.API.Match;
+import static io.vavr.Predicates.isIn;
 
 /**
  * <br/>
@@ -28,15 +31,21 @@ public class MatcherTest {
         Option<String> zero = Match(i).option(Case($(0), "zero"));
         System.out.println(zero.isDefined());
 
+        // 等值匹配
         String s1 = Match(i).of(
-                Case($(is(1)), "one"),
-                Case($(is(2)), "two"),
+                Case($(Predicate.isEqual(1)), "one"),
+                Case($(Predicate.isEqual(2)), "two"),
                 Case($(), "?")
         );
         System.out.println(s1);
+
+        // 断言匹配
+        UnaryOperator<String> hasValue = (input) -> Match(input).of(
+                Case($(isIn("h", "help")), "has value"),
+                Case($(), "not value")
+        );
+        System.out.println("null = " + hasValue.apply(null));
+        System.out.println("null = " + hasValue.apply("help"));
     }
 
-    private static Predicate<? super Integer> is(int i) {
-        return Predicate.isEqual(i);
-    }
 }
